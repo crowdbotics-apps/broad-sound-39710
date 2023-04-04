ea0404fd037427a34a043330a2831d7560e46202
Crowdbotics <team@crowdbotics.com>
2023-04-04 18:56:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broad-sound-39710",
+  url: "https://broad-sound-39710.botics.co",
+  api: "https://broad-sound-39710.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
